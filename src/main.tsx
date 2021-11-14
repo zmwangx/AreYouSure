ceb0fd8328f2547c86cfffe6d0646e5b3982cac7
Zhiming Wang <i@zhimingwang.org>
2021-11-14 22:42:09 +0800

    main: log repository link to console for visiting developers

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,11 @@ import { HashRouter, MemoryRouter } from 'react-router-dom';
 import App from '@/App';
 import '@/index.css';
 
+console.log(
+  '%cSource code: https://github.com/zmwangx/AreYouSure',
+  'color: #f97316; font-size: 20px'
+);
+
 // Use HashRouter in dev mode unless VITE_MEMORY_ROUTER is set, so that we can
 // freely refresh and test each route without playing the entire damn game.
 //
